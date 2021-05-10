34a024ea0b995f253fd228957519e312bbdf6dd3
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-10 16:01:32 +0700

    fix: className props overriding on CustomLink

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -3,8 +3,8 @@ import UnstyledLink from './UnstyledLink';
 export default function CustomLink(props) {
   return (
     <UnstyledLink
-      className={`${props.className}  inline-flex items-center font-bold hover:text-primary-400`}
       {...props}
+      className={`${props.className}  inline-flex items-center font-bold hover:text-primary-400`}
     >
       {props.children}
     </UnstyledLink>
