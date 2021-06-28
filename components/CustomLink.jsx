f02bbb52040692e558d9ad1868f4aeb8ca415788
Theodorus Clarence <theodorusclarence@gmail.com>
2021-06-28 07:55:43 +0700

    fix: spreading rest mess up autoimport

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -1,10 +1,12 @@
 import UnstyledLink from './UnstyledLink';
 
-export default function CustomLink({ className = '', ...rest }) {
+export default function CustomLink({ children, className = '', ...rest }) {
   return (
     <UnstyledLink
       {...rest}
       className={`inline-flex items-center font-bold hover:text-primary-400 animated-underline ${className}`}
-    />
+    >
+      {children}
+    </UnstyledLink>
   );
 }
--- components/UnstyledLink.jsx
+++ components/UnstyledLink.jsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 
-export default function UnstyledLink({ href, ...rest }) {
+export default function UnstyledLink({ children, href, ...rest }) {
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a {...rest} />
+        <a {...rest}>{children}</a>
       </Link>
     );
   }
 
-  return <a target='_blank' rel='noopener noreferrer' href={href} {...rest} />;
+  return (
+    <a target='_blank' rel='noopener noreferrer' href={href} {...rest}>
+      {children}
+    </a>
+  );
 }
