491e81196b6ef9846e105db430f63136e0a8c65b
Theodorus Clarence <theodorusclarence@gmail.com>
2021-06-28 08:02:17 +0700

    feat: add classnames helper

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -1,10 +1,14 @@
+import { classNames } from '@/lib/helper';
 import UnstyledLink from './UnstyledLink';
 
 export default function CustomLink({ children, className = '', ...rest }) {
   return (
     <UnstyledLink
       {...rest}
-      className={`inline-flex items-center font-bold hover:text-primary-400 animated-underline ${className}`}
+      className={classNames(
+        'inline-flex items-center font-bold hover:text-primary-400 animated-underline',
+        className
+      )}
     >
       {children}
     </UnstyledLink>
