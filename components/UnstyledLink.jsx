52177dcab499bacd9e948b43d27655b8bd4884d4
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-06 21:40:59 +0700

    feat: add cursor-ne-resize to external link

--- components/UnstyledLink.jsx
+++ components/UnstyledLink.jsx
@@ -1,18 +1,28 @@
 import Link from 'next/link';
 
-export default function UnstyledLink({ children, href, ...rest }) {
+import { classNames } from '@/lib/helper';
+
+export default function UnstyledLink({ children, href, className, ...rest }) {
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a {...rest}>{children}</a>
+        <a {...rest} className={className}>
+          {children}
+        </a>
       </Link>
     );
   }
 
   return (
-    <a target='_blank' rel='noopener noreferrer' href={href} {...rest}>
+    <a
+      target='_blank'
+      rel='noopener noreferrer'
+      href={href}
+      {...rest}
+      className={classNames(className, 'cursor-[ne-resize]')}
+    >
       {children}
     </a>
   );
