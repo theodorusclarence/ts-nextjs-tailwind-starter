bf64b7ccc3e0819fe0b3c6b8209b90eaa3923f56
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-08 15:42:20 +0700

    feat: add default styles for Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,28 @@
 import clsx from 'clsx';
 
 type ButtonProps = {
-  href: string;
   children: React.ReactChild | string;
   className?: string;
+  variant?: 'primary' | 'secondary';
 } & React.ComponentPropsWithoutRef<'button'>;
 
 export default function Button({
   children,
   className = '',
+  variant = 'primary',
   ...rest
 }: ButtonProps) {
   return (
     <button
       {...rest}
       className={clsx(
-        'inline-flex items-center font-bold hover:text-primary-500 animated-underline',
+        'py-2 px-4 rounded font-bold hover:text-primary-400 animated-underline',
+        'border border-gray-600',
+        {
+          'bg-dark text-white': variant === 'primary',
+          'bg-white text-dark hover:bg-gray-200 hover:text-dark transition-colors':
+            variant === 'secondary',
+        },
         className
       )}
     >
