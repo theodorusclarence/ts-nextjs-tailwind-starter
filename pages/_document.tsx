d2e0225a74982e2cafbf86a7e1a2980227cd3867
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-09 10:03:29 +0700

    feat: change to strict mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
