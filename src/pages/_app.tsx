72ce911b4dde75ff7bd8145a8592a433fd868c32
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-12 19:58:40 +0700

    style: remove unused fragment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,7 @@ import { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
