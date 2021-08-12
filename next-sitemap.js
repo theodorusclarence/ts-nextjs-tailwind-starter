339226c24cdebc6b45861c9232296bd89ee40633
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-12 20:08:18 +0700

    feat: add next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://ts-nextjs-tailwind-starter.theodorusclarence.com/',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
