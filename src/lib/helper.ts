b7848d4442071db3ea23b75eacd9dd9529ae2d15
Theodorus Clarence <theodorusclarence@gmail.com>
2021-10-10 20:55:37 +0700

    feat: add dynamic og from defaultMeta

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,9 @@
 export function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(' ');
 }
+
+export function openGraph(title: string, description: string): string {
+  const ogTitle = encodeURIComponent(title.trim());
+  const ogDesc = encodeURIComponent(description.trim());
+  return `https://thcl.dev/og?title=${ogTitle}&description=${ogDesc}`;
+}
