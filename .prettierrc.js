3eff8395f7d9b8a93ab3309fde1182e691b288aa
Theodorus Clarence <theodorusclarence@gmail.com>
2021-12-01 12:41:02 +0700

    chore: add prettier class prefix variant

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,4 +5,6 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   sortClassNamesUnknownClassesSeparator: '',
+  sortClassNamesPrefixes:
+    'xs:,sm:,md:,lg:,xl:,2xl:,dark:,motion-safe:,motion-reduce:,first:,last:,odd:,even:,visited:,checked:,group-hover:,group-focus:,focus-within:,hover:,focus:,focus-visible:,active:,disabled:',
 };
