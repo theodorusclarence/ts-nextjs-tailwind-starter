7752ffcf57ee26b4c0c2d844eb301f718c1f7dce
Theodorus Clarence <theodorusclarence@gmail.com>
2021-12-02 12:38:09 +0700

    feat: change CustomLink default ring

--- src/components/links/CustomLink.tsx
+++ src/components/links/CustomLink.tsx
@@ -6,15 +6,15 @@ import UnstyledLink, {
 
 export default function CustomLink({
   children,
-  className = '',
+  className,
   ...rest
 }: UnstyledLinkProps) {
   return (
     <UnstyledLink
       {...rest}
       className={clsx(
-        'animated-underline custom-link inline-flex items-center font-bold',
-        'ring-primary-400/70 focus:outline-none focus-visible:ring',
+        'animated-underline custom-link inline-flex items-center font-semibold',
+        'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500',
         'border-dark border-b border-dotted hover:border-black/0',
         className
       )}
