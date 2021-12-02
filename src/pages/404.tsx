89086d0b7a3232f2c82a3c8428d8fa75fa067a5b
Theodorus Clarence <theodorusclarence@gmail.com>
2021-12-02 13:50:49 +0700

    feat: improve 404 color and font size

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import Layout from '@/components/layout/Layout';
-import CustomLink from '@/components/links/CustomLink';
+import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
@@ -15,12 +15,12 @@ export default function NotFoundPage() {
           <div className='layout flex flex-col justify-center items-center min-h-screen text-center text-black'>
             <RiAlarmWarningFill
               size={60}
-              className='animate-flicker drop-shadow-glow text-yellow-600'
+              className='animate-flicker drop-shadow-glow text-red-500'
             />
-            <h1 className='mt-8'>Page Not Found</h1>
-            <CustomLink className='mt-4' href='/'>
+            <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
+            <ArrowLink className='mt-4 md:text-lg' href='/'>
               Back to Home
-            </CustomLink>
+            </ArrowLink>
           </div>
         </section>
       </main>
