536030dd538a2e2f7558d9af81176376176dfc4b
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-01 14:05:50 +0700

    chore: add clsxm config to class sorter

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,4 +7,5 @@ module.exports = {
   sortClassNamesUnknownClassesSeparator: '',
   sortClassNamesPrefixes:
     'xs:,sm:,md:,lg:,xl:,2xl:,dark:,motion-safe:,motion-reduce:,first:,last:,odd:,even:,visited:,checked:,group-hover:,group-focus:,focus-within:,hover:,focus:,focus-visible:,active:,disabled:',
+  sortClassNamesSortFunctions: 'clsx,classNames,cx,clsxm',
 };
