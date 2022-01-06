6ef698bfe343b22a334d9cce81f46819b9aabab1
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-06 19:53:47 +0700

    feat: add forward ref to links

--- src/components/links/PrimaryLink.tsx
+++ src/components/links/PrimaryLink.tsx
@@ -6,22 +6,23 @@ import UnstyledLink, {
   UnstyledLinkProps,
 } from '@/components/links/UnstyledLink';
 
-export default function PrimaryLink({
-  className,
-  children,
-  ...rest
-}: UnstyledLinkProps) {
-  return (
-    <UnstyledLink
-      {...rest}
-      className={clsxm(
-        'inline-flex items-center',
-        'font-medium text-primary-600 hover:text-primary-500',
-        'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
-        className
-      )}
-    >
-      {children}
-    </UnstyledLink>
-  );
-}
+const PrimaryLink = React.forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
+  ({ className, children, ...rest }, ref) => {
+    return (
+      <UnstyledLink
+        ref={ref}
+        {...rest}
+        className={clsxm(
+          'inline-flex items-center',
+          'font-medium text-primary-600 hover:text-primary-500',
+          'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
+          className
+        )}
+      >
+        {children}
+      </UnstyledLink>
+    );
+  }
+);
+
+export default PrimaryLink;
