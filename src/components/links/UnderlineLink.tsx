6e0b60f1d76a47ab08ad2daf32c0bc0272d865b9
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-06 19:53:47 +0700

    feat: add forward ref to links

--- src/components/links/UnderlineLink.tsx
+++ src/components/links/UnderlineLink.tsx
@@ -6,22 +6,23 @@ import UnstyledLink, {
   UnstyledLinkProps,
 } from '@/components/links/UnstyledLink';
 
-export default function UnderlineLink({
-  children,
-  className,
-  ...rest
-}: UnstyledLinkProps) {
-  return (
-    <UnstyledLink
-      {...rest}
-      className={clsxm(
-        'animated-underline custom-link inline-flex items-center font-semibold',
-        'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
-        'border-dark border-b border-dotted hover:border-black/0',
-        className
-      )}
-    >
-      {children}
-    </UnstyledLink>
-  );
-}
+const UnderlineLink = React.forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
+  ({ children, className, ...rest }, ref) => {
+    return (
+      <UnstyledLink
+        ref={ref}
+        {...rest}
+        className={clsxm(
+          'animated-underline custom-link inline-flex items-center font-semibold',
+          'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
+          'border-dark border-b border-dotted hover:border-black/0',
+          className
+        )}
+      >
+        {children}
+      </UnstyledLink>
+    );
+  }
+);
+
+export default UnderlineLink;
--- src/components/links/UnstyledLink.tsx
+++ src/components/links/UnstyledLink.tsx
@@ -1,4 +1,5 @@
 import Link, { LinkProps } from 'next/link';
+import * as React from 'react';
 
 import clsxm from '@/lib/clsxm';
 
@@ -8,40 +9,38 @@ export type UnstyledLinkProps = {
   openNewTab?: boolean;
   className?: string;
   nextLinkProps?: Omit<LinkProps, 'href'>;
-} & React.ComponentPropsWithoutRef<'a'>;
+} & React.ComponentPropsWithRef<'a'>;
 
-export default function UnstyledLink({
-  children,
-  href,
-  openNewTab,
-  className,
-  nextLinkProps,
-  ...rest
-}: UnstyledLinkProps) {
-  const isNewTab =
-    openNewTab !== undefined
-      ? openNewTab
-      : href && !href.startsWith('/') && !href.startsWith('#');
+const UnstyledLink = React.forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
+  ({ children, href, openNewTab, className, nextLinkProps, ...rest }, ref) => {
+    const isNewTab =
+      openNewTab !== undefined
+        ? openNewTab
+        : href && !href.startsWith('/') && !href.startsWith('#');
 
-  if (!isNewTab) {
+    if (!isNewTab) {
+      return (
+        <Link href={href} {...nextLinkProps}>
+          <a ref={ref} {...rest} className={className}>
+            {children}
+          </a>
+        </Link>
+      );
+    }
+
     return (
-      <Link href={href} {...nextLinkProps}>
-        <a {...rest} className={className}>
-          {children}
-        </a>
-      </Link>
+      <a
+        ref={ref}
+        target='_blank'
+        rel='noopener noreferrer'
+        href={href}
+        {...rest}
+        className={clsxm('cursor-newtab', className)}
+      >
+        {children}
+      </a>
     );
   }
+);
 
-  return (
-    <a
-      target='_blank'
-      rel='noopener noreferrer'
-      href={href}
-      {...rest}
-      className={clsxm('cursor-newtab', className)}
-    >
-      {children}
-    </a>
-  );
-}
+export default UnstyledLink;
