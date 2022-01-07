1677dda8df26a5f5f5156f8a875dabec9c31203a
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-07 19:08:09 +0700

    feat: add support for svgr
    no type caveat on the svg jsx

--- next.config.js
+++ next.config.js
@@ -12,4 +12,23 @@ module.exports = {
   //     'res.cloudinary.com',
   //   ],
   // },
+
+  // SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
