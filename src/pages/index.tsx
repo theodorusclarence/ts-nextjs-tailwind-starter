a742bd336c03d68f53ff6a9c3fb4d8820775fef5
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-07 19:08:09 +0700

    feat: add support for svgr
    no type caveat on the svg jsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,15 @@ import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Vercel from '~/svg/Vercel.svg';
+
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
@@ -20,7 +29,10 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex flex-col justify-center items-center min-h-screen text-center'>
-            <h1>Next.js + Tailwind CSS + TypeScript Starter</h1>
+            <Vercel className='text-5xl' />
+            <h1 className='mt-4'>
+              Next.js + Tailwind CSS + TypeScript Starter
+            </h1>
             <p className='mt-2 text-sm text-gray-800'>
               A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
               Import, Seo, Link component, pre-configured with Husky{' '}
