753a1a838f8b17dc97973dac328add3571473fde
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-25 17:08:13 +0700

    feat: use first class class sorter

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,8 +4,4 @@ module.exports = {
   jsxSingleQuote: true,
   tabWidth: 2,
   semi: true,
-  sortClassNamesUnknownClassesSeparator: '',
-  sortClassNamesPrefixes:
-    'xs:,sm:,md:,lg:,xl:,2xl:,dark:,motion-safe:,motion-reduce:,first:,last:,odd:,even:,visited:,checked:,group-hover:,group-focus:,focus-within:,hover:,focus:,focus-visible:,active:,disabled:',
-  sortClassNamesSortFunctions: 'clsx,classNames,cx,clsxm',
 };
