c89e6bc2e731aa26d4c2a8885b1475322a4c906f
Theodorus Clarence <theodorusclarence@gmail.com>
2022-02-13 21:13:08 +0700

    fix: layout declared twice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,13 @@ import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
 
-import Layout from '@/components/layout/Layout';
+/**
+ * !STARTERCONF info
+ * ? `Layout` component is called in every page using `np` snippets. If you have consisten layout across all page, you can add it here too
+ */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
