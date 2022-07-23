8764bfd948e21267d1fc824028499933b9b2fb9b
Theodorus Clarence <theodorusclarence@gmail.com>
2022-07-23 13:36:55 +0700

    feat: change buttons font to medium

--- src/components/links/ButtonLink.tsx
+++ src/components/links/ButtonLink.tsx
@@ -29,7 +29,7 @@ const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
         ref={ref}
         {...rest}
         className={clsxm(
-          'inline-flex items-center rounded px-4 py-2 font-semibold',
+          'inline-flex items-center rounded px-4 py-2 font-medium',
           'focus:outline-none focus-visible:ring focus-visible:ring-primary-500',
           'shadow-sm',
           'transition-colors duration-75',
