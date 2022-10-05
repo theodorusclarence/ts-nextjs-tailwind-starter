7f8af265162c6e81d46b8b5336b1034ad47f4056
Theodorus Clarence <theodorusclarence@gmail.com>
2022-10-05 10:22:23 +0700

    ci: turn off unescaped entities eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-unused-vars': 'off',
     'no-console': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/no-unescaped-entities': 'off',
 
     'react/display-name': 'off',
     'react/jsx-curly-brace-presence': [
