0725f8a7a7c090db6efc303b2dc187860eb839a7
Austin Langdon <alangdon93@gmail.com>
2022-10-26 05:48:42 +0000

    fix: add html lang and loading comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang='en'>
       <head></head>
       <body>
         <Header />
---
+++ src/app/test/loading.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
