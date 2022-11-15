a027625542f1464f3f2e2c37ca21d844ea577109
Theodorus Clarence <theodorusclarence@gmail.com>
2022-11-15 11:48:41 +0700

    feat: change favicon generator default

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -83,74 +83,19 @@ export default function Seo(props: SeoProps) {
         <link key={linkProps.href} {...linkProps} />
       ))}
       <meta name='msapplication-TileColor' content='#ffffff' />
-      <meta
-        name='msapplication-TileImage'
-        content='/favicon/ms-icon-144x144.png'
-      />
+      <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
       <meta name='theme-color' content='#ffffff' />
     </Head>
   );
 }
 
-type Favicons = {
-  rel: string;
-  href: string;
-  sizes?: string;
-  type?: string;
-};
-
-// !STARTERCONF this is the default favicon, you can generate your own from https://www.favicon-generator.org/ then replace the whole /public/favicon folder
-const favicons: Array<Favicons> = [
-  {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/favicon/apple-icon-152x152.png',
-  },
+// !STARTERCONF this is the default favicon, you can generate your own from https://realfavicongenerator.net/
+// ! then replace the whole /public/favicon folder and favicon.ico
+const favicons: Array<React.ComponentPropsWithoutRef<'link'>> = [
   {
     rel: 'apple-touch-icon',
     sizes: '180x180',
-    href: '/favicon/apple-icon-180x180.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'icon',
@@ -161,17 +106,14 @@ const favicons: Array<Favicons> = [
   {
     rel: 'icon',
     type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
     sizes: '16x16',
     href: '/favicon/favicon-16x16.png',
   },
+  { rel: 'manifest', href: '/favicon/site.webmanifest' },
   {
-    rel: 'manifest',
-    href: '/favicon/manifest.json',
+    rel: 'mask-icon',
+    href: '/favicon/safari-pinned-tab.svg',
+    color: '#00e887',
   },
+  { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
 ];
