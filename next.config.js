0d91aa735674ecf5be85671a33f436bdbc363319
Theodorus Clarence <theodorusclarence@gmail.com>
2023-07-17 17:55:08 +0700

    feat: migrate to app directory

--- next.config.js
+++ next.config.js
@@ -13,25 +13,6 @@ const nextConfig = {
   //     'res.cloudinary.com',
   //   ],
   // },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next';
+import * as React from 'react';
+import { RiAlarmWarningFill } from 'react-icons/ri';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+};
+
+export default function NotFound() {
+  return (
+    <main>
+      <section className='bg-white'>
+        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+          <RiAlarmWarningFill
+            size={60}
+            className='drop-shadow-glow animate-flicker text-red-500'
+          />
+          <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
+          <a href='/'>Back to home</a>
+        </div>
+      </section>
+    </main>
+  );
+}
