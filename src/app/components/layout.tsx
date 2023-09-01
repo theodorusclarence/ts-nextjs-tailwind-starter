780bf563c96edc8bebb43c9b8fc27c5bdbeb4f83
Keith Baker <github@baker.af>
2023-09-01 12:55:18 -0400

    fix: added missing css file to show style changes on component page

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import * as React from 'react';
 
+import '@/styles/colors.css';
+
 export const metadata: Metadata = {
   title: 'Components',
   description: 'Pre-built components with awesome default',
