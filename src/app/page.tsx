d1d26b88b5fb33c904adf341b74b73c288149e73
gmguarino <gmguarino1@gmail.com>
2024-03-14 16:15:01 +0100

    app skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import Head from 'next/head';
+import Link from 'next/link';
 import * as React from 'react';
 import '@/lib/env';
 
+import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/layout/Navbar';
 import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
+
+import Logo from '~/svg/Logo.svg';
+import TextButton from '@/components/buttons/TextButton';
 
 /**
  * SVGR Support
@@ -16,57 +22,114 @@ import UnstyledLink from '@/components/links/UnstyledLink';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Logo from '~/svg/Logo.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
-export default function HomePage() {
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <IntroSection />
+      <BombSection />
+      <ExplodeSection />
+      <BioSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
+
+const IntroSection = () => {
   return (
     <main>
       <Head>
         <title>Hi</title>
       </Head>
-      <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
-
-          <ButtonLink className='mt-6' href='/components' variant='light'>
-            See all components
-          </ButtonLink>
-
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
-            />
-          </UnstyledLink>
-
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
+      <section className='bg-rose1'>
+        <div className='relative flex w-full items-end ps-10 pb-20 bg-cover bg-center bg-no-repeat lg:min-h-screen'
+        style={{
+          backgroundImage: "url('/svg/page_1.svg')",
+        }}
+        >
+          <div className="text-7xl items-left font-bold text-orange2 w-3/4 md:w-full lg:w-3/4">
+            LE SAUMON QUE VOUS MANGEZ, VENDEZ, PRODUISEZ ...
+          </div>
         </div>
       </section>
     </main>
   );
 }
+
+const BombSection = () => {
+  return (
+    <main>
+      <Head>
+        <title>Hi</title>
+      </Head>
+      <section className='bg-fuschia1'>
+        <div className='relative flex w-full items-end ps-10 pb-20 bg-cover bg-center bg-no-repeat lg:min-h-screen'
+        style={{
+          backgroundImage: "url('/svg/page_2.svg')",
+        }}
+        >
+          <div className="text-7xl items-left font-bold text-orange2 w-1/2 md:w-full lg:w-1/2">
+            TUE AFFAME ET CONTAMINE.
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+const ExplodeSection = () => {
+  return (
+    <main>
+      <Head>
+        <title>Hi</title>
+      </Head>
+      <section className='bg-fuschia1'>
+        <div className='relative flex w-full items-end bg-contain bg-fuschia1 bg-center bg-no-repeat lg:min-h-screen'
+        style={{
+          backgroundImage: "url('/svg/page_3.svg')",
+        }}
+        >
+        </div>
+      </section>
+    </main>
+  );
+}
+const BioSection = () => {
+  return (
+    <div className='grid grid-cols-1 lg:min-h-screen lg:grid-cols-2 bg-fuschia1 text-orange2'>
+      <section className='hero-section lg:px-30 container mx-auto px-8 py-16 pt-24 lg:self-center lg:px-20 lg:pt-16'>
+      <div>
+
+      </div>
+      </section>
+      <section className='hero-section lg:px-30 container mx-auto px-8 py-16 pt-24 lg:self-center lg:px-20 lg:pt-16'>
+        <h2 className='mb-12 text-4xl font-bold lg:text-6xl'>
+          BIODIVERSITE
+        </h2>
+
+        <div className='flex flex-col gap-8 lg:gap-2 w-3/5'>
+          <p>
+            L'élevage intensif de saumon a des répercussions significatives sur la biodiversité marine, 
+            avec des échappées, une pollution de l'eau par les rejets, des parasites affectant les populations sauvages,
+            et une pression accrue sur les stocks de poissons sauvages due à la demande de farine de poisson, 
+            contribuant ainsi à la surexploitation et à la diminution de la biodiversité.
+          </p>
+          <Link
+            className='block border-2 bg-fuschia1 px-4 py-2 text-center text-xl text-orange2 border-orange2 transition duration-50 hover:text-orange1 lg:inline-block'
+            href="/"
+            >
+            <b>EN SAVOIR PLUS</b>
+          </Link>
+        </div>
+      </section>
+      
+    </div>
+  );
+};
