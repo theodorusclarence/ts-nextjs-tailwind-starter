6c0a89bcdedbc9ded122817d2605e66e532dbe3e
gmguarino <gmguarino1@gmail.com>
2024-03-22 10:41:59 +0100

    add image card

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,15 @@
+import clsx from 'clsx';
+import React, { ReactNode } from 'react'
+
+const ImageCard = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className={clsx('rounded-xl bg-transparent', className)}>{children}</div>
+  );
+};
+export default ImageCard
