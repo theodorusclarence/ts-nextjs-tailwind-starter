912596a6bc01437c9ffb7155141d02838aa1e23d
Malena Guallar <malena.guallar@gmail.com>
2024-03-27 15:25:24 +0100

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ const HomePage = () => {
         */}
       </main>
       
-      {/*<Footer />*/}
+      <Footer />
     </>
   );
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,119 +2,120 @@ import clsx from 'clsx';
 import Link from 'next/link';
 
 const Footer = () => (
-  <footer className='bg-orange2'>
-    <div className='p-5'>
+  <footer className='bg-orange2 '>
+    <div className='px-6 md:px-12 py-6 md:py-7'>
       
-      <h1 className='space-y-5 text-left text-7xl lg:px-20 bg-orange2 text-darkblue1'>
-            <b>PARLONS - EN !</b>
+      <h1 className='flex justify-center md:justify-start bg-orange2 text-darkblue1'>
+            <b className='text-4xl md:text-6xl'>PARLONS - EN !</b>
       </h1>
     </div>
-    <div className='p-5'>
-      <img className='h-auto max-w-lg mx-auto' src='/svg/fish_eye.svg' alt='' />
+    <div className='flex justify-center px-6 md:px-12 py-3 md:py-7'>
+      <img src='/svg/fish_eye.svg' alt='' />
     </div>
-    <div className='container mx-auto grid gap-10 px-8 lg:px-20 pb-20 pt-16 sm:grid-cols-1 md:grid-cols-[1fr_1fr_1fr_1fr] lg:gap-10'>
-      <div className='general-section flex flex-col gap-5'>
+    <div className='flex flex-col px-8 md:px-20 pb-20 pt-16'>
+      <div className='flex flex-col gap-5'>
         
-        <p className='text-center lg:text-left'>
+        <p className='text-center md:text-left '>
           Description de Seastemik et Data4Good
         </p>
-        <div className={clsx('footer__cta-wrapper', 'flex flex-col gap-4')}>
+        <div className={clsx('footer__cta-wrapper', 'flex flex-col gap-4 pb-10 md:grid md:grid-cols-2 md:gap-32' )}>
           <Link
-            className='block rounded bg-black px-4 py-2 text-center text-white transition duration-100 hover:bg-gray-700 lg:inline-block'
+            className='block rounded bg-black px-4 py-2 text-center text-white transition duration-100 hover:bg-gray-700 md:col-start-1 '
             href="/"
           >
             <b>ACTION 1</b>
           </Link>
           <Link
-            className='block rounded bg-black px-4 py-2 text-center text-white transition duration-100 hover:bg-gray-700 lg:inline-block'
+            className='block rounded bg-black px-4 py-2 text-center text-white transition duration-100 hover:bg-gray-700 md:col-start-2'
             href="/"
           >
             <b>ACTION 2</b>
           </Link>
         </div>
       </div>
-      <div className='seastemik-section flex flex-col items-center'>
-        <div>
-          <img className='mb-4 h-16' src='/images/seastemik-logo.png' alt='' />
+      <div className='md:pt-10 md:grid md:grid-cols-2'>
+        <div className='flex flex-col items-center py-5' >
+          <div>
+            <img className='mb-4 h-16' src='/images/seastemik-logo.png' alt='' />
+          </div>
+          <p className='mb-4 text-2xl'>
+            <b>Seastemik</b>
+          </p>
+          <div className='flex flex-col gap-2'>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://seastemik.org/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>Website</span>
+              <img src='/svg/globe.svg' alt='' />
+            </a>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://twitter.com/seastemik'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>Twitter</span>
+              <img src='/svg/x-logo.svg' alt='' />
+            </a>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://www.linkedin.com/company/seastemik-org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>LinkedIn</span>
+              <img src='/svg/linkedin-logo.svg' alt='' />
+            </a>
+          </div>
         </div>
-        <p className='mb-4 text-2xl'>
-          <b>Seastemik</b>
-        </p>
-        <div className='flex flex-col gap-2'>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://seastemik.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>Website</span>
-            <img src='/svg/globe.svg' alt='' />
-          </a>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://twitter.com/seastemik'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>Twitter</span>
-            <img src='/svg/x-logo.svg' alt='' />
-          </a>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://www.linkedin.com/company/seastemik-org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>LinkedIn</span>
-            <img src='/svg/linkedin-logo.svg' alt='' />
-          </a>
+        <div className='flex flex-col items-center py-5 pb-20 '>
+          <div>
+            <img className='mb-4' src='/images/d4g-logo.png' alt='' />
+          </div>
+          <p className='mb-4 text-2xl'>
+            <b>Data for Good</b>
+          </p>
+          <div className='flex flex-col gap-2'>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://dataforgood.fr/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>Website</span>
+              <img src='/svg/globe.svg' alt='' />
+            </a>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://twitter.com/dataforgood_fr'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>Twitter</span>
+              <img src='/svg/x-logo.svg' alt='' />
+            </a>
+            <a
+              className='flex w-[110px] justify-between'
+              href='https://www.linkedin.com/company/dataforgood'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>LinkedIn</span>
+              <img src='/svg/linkedin-logo.svg' alt='' />
+            </a>
+          </div>
         </div>
       </div>
-      <div className='d4g-section flex flex-col items-center'>
-        <div>
-          <img className='mb-4' src='/images/d4g-logo.png' alt='' />
-        </div>
-        <p className='mb-4 text-2xl'>
-          <b>Data for Good</b>
-        </p>
-        <div className='flex flex-col gap-2'>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://dataforgood.fr/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>Website</span>
-            <img src='/svg/globe.svg' alt='' />
-          </a>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://twitter.com/dataforgood_fr'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>Twitter</span>
-            <img src='/svg/x-logo.svg' alt='' />
-          </a>
-          <a
-            className='flex w-[110px] justify-between'
-            href='https://www.linkedin.com/company/dataforgood'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <span>LinkedIn</span>
-            <img src='/svg/linkedin-logo.svg' alt='' />
-          </a>
-        </div>
-      </div>
-      <div className='general-section flex flex-col gap-5 justify-center'>
-        
-        <p className='text-center lg:text-left'>
-          All rights reserved © 2024 Seastemik and Data for Good
+      <div className='flex justify-center md:justify-end m-0 p-0'>
+        <p className='text-center self-end md:text-right m-0 p-0'>
+          All rights reserved <br/> © 2024 Seastemik and Data for Good
         </p>
       </div>
     </div>
   </footer>
 );
-
+ 
 export default Footer;
