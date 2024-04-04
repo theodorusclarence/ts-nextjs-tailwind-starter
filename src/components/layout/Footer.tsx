929d0858dac4a4e525eb2e0c8f6f2a1ff66d6683
Thomas Catinaud Taris <tcatinaud-taris@greenspector.com>
2024-04-04 22:14:49 +0200

    refactor: footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,91 +1,32 @@
+import ShareLink from "@/components/links/shareLink";
+
 const currentYear = new Date().getFullYear();
 
 const Footer = () => (
-  <footer className='bg-orange2 '>
-    <div className='px-6 md:px-12 py-6 md:py-7 pt-0'>
-      
-      <div className='md:grid md:grid-cols-2'>
-        <div className='flex flex-col items-center py-5' >
-          <div>
-            <img className='mb-4 h-16' src='/images/seastemik-logo.png' alt='' />
-          </div>
-          <p className='mb-4 text-2xl'>
-            <b>Seastemik</b>
-          </p>
-          <div className='flex flex-col gap-2'>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://seastemik.org/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>Website</span>
-              <img src='/svg/globe.svg' alt='' />
-            </a>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://twitter.com/seastemik'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>Twitter</span>
-              <img src='/svg/x-logo.svg' alt='' />
-            </a>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://www.linkedin.com/company/seastemik-org'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>LinkedIn</span>
-              <img src='/svg/linkedin-logo.svg' alt='' />
-            </a>
-          </div>
-        </div>
-        <div className='flex flex-col items-center py-5 pb-20 '>
-          <div>
-            <img className='mb-4' src='/images/d4g-logo.png' alt='' />
-          </div>
-          <p className='mb-4 text-2xl'>
-            <b>Data for Good</b>
-          </p>
-          <div className='flex flex-col gap-2'>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://dataforgood.fr/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>Website</span>
-              <img src='/svg/globe.svg' alt='' />
-            </a>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://twitter.com/dataforgood_fr'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>Twitter</span>
-              <img src='/svg/x-logo.svg' alt='' />
-            </a>
-            <a
-              className='flex w-[110px] justify-between'
-              href='https://www.linkedin.com/company/dataforgood'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span>LinkedIn</span>
-              <img src='/svg/linkedin-logo.svg' alt='' />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className='flex justify-center md:justify-end m-0 p-0'>
-        <p className='text-center self-end md:text-right m-0 p-0'>
-          All rights reserved <br/> © {currentYear} Seastemik and Data for Good
-        </p>
-      </div>
+  <footer className='flex flex-wrap gap-x-4 gap-y-12 justify-center p-6 lg:p-12 text-white bg-darkblue1'>
+    <div className='flex flex-col items-center w-full max-w-64'>
+      <img className='mb-4 h-16' src='/images/seastemik-logo.png' alt='' width={64} height={64} />
+      <p className='mb-4 lg:text-2xl font-bold'>
+        Seastemik
+      </p>
+      <ShareLink href='https://seastemik.org/' name='Seastemik' />
+      <ShareLink type='twitter' href='https://twitter.com/seastemik' name='Seastemik' />
+      <ShareLink type='linkedin' href='https://www.linkedin.com/company/seastemik-org' name='Seastemik' />
     </div>
+
+    <div className='flex flex-col items-center w-full max-w-64'>
+      <img className='mb-4 h-16' src='/images/dfg-logo.png' alt='' width={64} height={64} />
+      <p className='mb-4 lg:text-2xl font-bold'>
+        Data for Good
+      </p>
+      <ShareLink href='https://dataforgood.fr/' name='Data for Good' />
+      <ShareLink type='twitter' href='https://twitter.com/dataforgood_fr' name='Data for Good' />
+      <ShareLink type='linkedin' href='https://www.linkedin.com/company/dataforgood' name='Data for Good' />
+    </div>
+
+    <p className='w-full text-center'>
+      Tous droits résérvés <br/> © {currentYear} Seastemik et Data for Good
+    </p>
   </footer>
 );
  
---
+++ src/components/links/shareLink.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx';
+import React from 'react'
+import Image from 'next/image';
+
+const social:Record<string, {name: string, icon: string}> = {
+  twitter: {
+    name: 'Twitter',
+    icon: 'x-logo',
+  },
+  linkedin: {
+    name: 'Linkedin',
+    icon: 'linkedin-logo',
+  },
+  website: {
+    name: 'Site internet',
+    icon: 'globe',
+  },
+}
+
+type ShareLinkProps = {
+  className?: string;
+  type?: keyof typeof social;
+  href: string;
+  name: string;
+};
+
+const ShareLink = ({
+  className,
+  type='website',
+  href,
+  name,
+  ...rest
+}:ShareLinkProps) => {
+  if (!href || !name) {
+    return <></>;
+  }
+
+  return (
+    <a
+      className={clsx('flex mt-2 gap-2 items-center hover:underline', className)}
+      href={href}
+      target='_blank'
+      rel='noopener noreferrer'
+      title={`${social[type].name} ${name} (nouvel onglet)`}
+      {...rest}
+    >
+      <img src={`/svg/${social[type].icon}.svg`} alt='' className='w-4' />
+      {social[type].name}
+      <span className='sr-only'>{name}</span>
+      <img src='/svg/new-tab.svg' alt='' className='w-4' />
+    </a>
+  );
+};
+
+export default ShareLink;
