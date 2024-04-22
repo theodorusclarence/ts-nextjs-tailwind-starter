a4d26829e11bc752fbf5d57d3a29339f13c6cc44
gmguarino <gmguarino1@gmail.com>
2024-04-22 13:41:41 +0200

    fix: change url

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
   title: 'Pinkbombs',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    "Analyzing the impact of Salmon on the Environment, Ecosystem, Health and Communities.",
+  url: "https://pinkbombs-app.vercel.app",
 };
