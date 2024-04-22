f643f06ee8c9cae03511fb91d0b2ee6524d42e83
gmguarino <gmguarino1@gmail.com>
2024-04-22 13:41:41 +0200

    fix: change url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: "https://tsnext-tw.thcl.dev",
+  siteUrl: "https://pinkbombs-app.vercel.app",
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: "*", allow: "/" }],
