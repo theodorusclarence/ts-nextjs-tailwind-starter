2d3b19d61290579cba6d62ef969476f2de7358cd
AkshayVoora <145536033+AkshayVoora@users.noreply.github.com>
2026-01-25 09:34:06 -0700

    changed site config

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'Chanaged Title affecting Metadata',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'CHanged desc. too affecting Metadata in mult places as a part of layout.tsx',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
